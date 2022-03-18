ec2504fd6d6c753fc44d92f57057ab3d5b1979f7
Deborah Pickard <96426109+DebPeekers@users.noreply.github.com>
2022-03-18 16:57:48 +1030

    Update ft_bzero.c

--- PART 1/ft_bzero.c
+++ PART 1/ft_bzero.c
@@ -10,9 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+*/
+*	The bzero() function writes n zeroed bytes to the string s.  
+*	If n is zero, bzero() does nothing.
+*	PARAMETERS
+*	#1. The pointer from which to begin erasing data.
+*	#2. The number of bytes to erase.
+*	RETURN VALUES
+*	- 
+*/
+
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
+	unsigned int	i;
+	char			*str_c;
+
+	str_c = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		*(str_c + i) = 0;
+		i++;
+	}
 }
