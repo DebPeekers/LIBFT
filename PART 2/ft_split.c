f4bce116ebfa0834633974ea7fab6457a9de37df
Deborah Pickard <96426109+DebPeekers@users.noreply.github.com>
2022-04-05 12:24:04 +0930

    Update ft_split.c

--- PART 2/ft_split.c
+++ PART 2/ft_split.c
@@ -2,92 +2,107 @@
 *	Allocates (with malloc(3)) and returns an array of strings obtained by 
 *	splitting ’s’ using the character ’c’ as a delimiter. The array must be
 *	ended by a NULL pointer.
-*
-*	#1. The string to be split.
-*	#2. The delimiter character.
-*	RETURN VALUES
-*	The array of new strings resulting from the split. 
-*	NULL if the allocation fails.
 */
-
 #include "libft.h"
 
-static int	ft_count_words(char const *str, char c)
+static void	ft_free2d(char *strs)
 {
 	int	i;
-	int	count;
 
 	i = 0;
-	count = 0;
-	while (str[i] != '\0')
+	while (strs[i] != '\0')
 	{
-		if (str[i] == c)
-			i++;
-		else
-		{
-			count++;
-			while (str[i] && str[i] != c)
-				i++;
-		}
+		free(strs);
+		i++;
 	}
-	return (count);
+	free(strs);
 }
 
-static char	*ft_putword(char *word, char const *s, int i, int word_len)
-{
-	int	j;
-
-	j = 0;
-	while (word_len > 0)
-	{
-		word[j] = s[i - word_len];
-		j++;
-		word_len--;
-	}
-	word[j] = '\0';
-	return (word);
-}
-
-static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
+static int	ft_wordcount(char *str, char c)
 {
 	int	i;
 	int	word;
-	int	word_len;
 
 	i = 0;
 	word = 0;
-	word_len = 0;
-	while (word < num_words)
+	while (str[i] != '\0')
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		while (s[i] && s[i] != c)
+		if (str[i] != c)
 		{
-			i++;
-			word_len++;
+			word++;
+			while (str[i] != c && str[i] != '\0')
+				i++;
+			if (str[i] == '\0')
+				return (word);
 		}
-		s2[word] = (char *)malloc(sizeof(char) * (word_len + 1));
-		if (!s2)
-			return (0);
-		ft_putword(s2[word], s, i, word_len);
-		word_len = 0;
-		word++;
+		i++;
 	}
-	s2[word] = 0;
-	return (s2);
+	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+static void	ft_strcpy(char *word, char *str, char c, int j)
 {
-	char			**s2;
-	unsigned int	num_words;
+	int	i;
 
-	if (!s)
-		return (0);
-	num_words = ft_count_words(s, c);
-	s2 = (char **)malloc(sizeof(char *) * (num_words + 1));
-	if (!s2)
-		return (0);
-	ft_split_words(s, c, s2, num_words);
-	return (s2);
+	i = 0;
+	while (str[j] != '\0' && str[j] == c)
+		j++;
+	while (str[j + i] != c && str[j + i] != '\0')
+	{
+		word[i] = str[j + i];
+		i++;
+	}
+	word[i] = '\0';
+}
+
+static char	*ft_stralloc(char *str, char c, int *k)
+{
+	char	*word;
+	int		j;
+
+	j = *k;
+	word = NULL;
+	while (str[*k] != '\0')
+	{
+		if (str[*k] != c)
+		{
+			while (str[*k] != '\0' && str[*k] != c)
+				*k += 1;
+			word = (char *)malloc(sizeof(char) * (*k + 1));
+			if (word == NULL)
+				return (NULL);
+			break ;
+		}
+		*k += 1;
+	}
+	ft_strcpy(word, str, c, j);
+	return (word);
+}
+
+char	**ft_split(char const *str, char c)
+{
+	char	**strs;
+	int		i;
+	int		j;
+	int		pos;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	pos = 0;
+	j = ft_wordcount((char *)str, c);
+	strs = (char **)malloc(sizeof(char *) * (j + 1));
+	if (strs == NULL)
+		return (NULL);
+	strs[j] = NULL;
+	while (i < j)
+	{
+		strs[i] = ft_stralloc(((char *)str), c, &pos);
+		if (strs[i] == NULL)
+		{
+			ft_free2d(strs[i]);
+		}
+		i++;
+	}
+	return (strs);
 }
