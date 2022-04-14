00a523d735db6e3c84bc17a5812a6b2a60cee7db
Deborah Pickard <96426109+DebPeekers@users.noreply.github.com>
2022-04-14 09:59:24 +0930

    Update ft_atoi.c

--- PART 1/ft_atoi.c
+++ PART 1/ft_atoi.c
@@ -5,38 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpickard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/02 09:03:53 by dpickard          #+#    #+#             */
-/*   Updated: 2022/03/02 12:08:04 by dpickard         ###   ########.fr       */
+/*   Created: 2022/03/30 10:32:35 by dpickard          #+#    #+#             */
+/*   Updated: 2022/04/07 10:33:49 by dpickard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-*	Converts a string into and integer.
-*	1. skips white-space characters at beginning of string,
-*	2. onverts subsequent characters as part of the number stoping when it encounters
-*	the first charager that isn't a number */
+FUNCTION CONVERTS STRING INTO INEGER
+SKIPS WHITE-SPACE AT START
+CONVERTS CHARACTERS AS PART OF NUMBER STOPING AT FIRST CHAR THAT ISNT A NUMBER 
+*/
 
-int ft_atoi(const char *str)
+#include "libft.h"
+
+int	ft_atoi(const char *str)
 {
-    int i;
-    int neg;
-    int res;
+	int	i;
+	int	neg;
+	int	res;
 
 i = 0;
 neg = 1;
 res = 0;
-while (str[i] == ' '|| (str[i] >= 9 && str[i] <= 13))
-		i++;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			neg *= -1;
-		i++;
+			i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str [i] <= '9')
 	{
 		res = (str[i] - '0') + (res * 10);
 		i++;
 	}
-	return (res * neg);)
+	return (res * neg);
 }
